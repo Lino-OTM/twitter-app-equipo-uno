587b2fc7f87574045068f8a2d5052cb2e287adbb
Adrian Rodriguez <rgadrian.93@gmail.com>
2024-06-18 10:35:39 -0300

    cors installation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const { expressjwt: checkJwt } = require("express-jwt");
 
-router.get("/", checkJwt({ secret: process.env.SECRET_JWT, algorithms: ["HS256"] }),userController.index);
+router.get("/", userController.index);
 router.post("/", userController.store);
 router.get("/:id", userController.show);
 router.patch("/:id", userController.update);
 router.delete("/:id", userController.destroy);
 
 module.exports = router;
+
+// checkJwt({ secret: process.env.SECRET_JWT, algorithms: ["HS256"] }),
--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const express = require("express");
 const routes = require("./routes");
 const port = process.env.APP_PORT;
 const app = express();
+const cors = require('cors');
 
-// app.use(cors()); // Para habilitar esta línea es necesario instalar la librería `cors`.
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cors());
 
 routes(app);
+
 
 app.listen(port, () => {
   console.log(`\n[Express] Servidor corriendo en el puerto ${port}.`);
