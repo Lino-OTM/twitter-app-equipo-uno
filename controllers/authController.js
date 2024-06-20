c0010a72712005ba473112ed6d07b330dd6eb5af
Adrian Rodriguez <rgadrian.93@gmail.com>
2024-06-20 09:25:05 -0300

    update auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,16 +3,22 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 
 async function getToken(req, res) {
-  const user = await User.findOne({ email: req.body.email });
-  if (!user) return res.json({ msg: "verifque credenciales 1..." });
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.json({ msg: "verifque credenciales 1..." });
 
-  const match = bcrypt.compare(req.body.password, user.password); //devuelve true or false
-  if (!match) return res.json({ msg: "verifique credenciales 2..." });
-  const token = jwt.sign({ sub: user.id, username: user.username }, process.env.SECRET_JWT, {
-    expiresIn: "1h",
-  });
-  res.json({ user, token });
-  console.log(token);
+    const match = bcrypt.compare(req.body.password, user.password); //devuelve true or false
+    if (!match) return res.json({ msg: "verifique credenciales 2..." });
+    const token = jwt.sign({ sub: user.id, username: user.username }, process.env.SECRET_JWT, {
+      expiresIn: "1h",
+    });
+
+    res.json({ token });
+  } catch {
+    (error) => {
+      res.json({ msg: "error en el servidor", error });
+    };
+  }
 }
 
 module.exports = { getToken };
